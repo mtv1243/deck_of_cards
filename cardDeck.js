fd863b3546c98b152287a11d58897215657e185e
maustin1 <mtv1243@gmail.com>
2020-09-11 00:05:04 -0400

    add functionality to the card elements. click a card in your hand to add it to either the pot or your crib. will update to the firebase as well.

--- cardDeck.js
+++ cardDeck.js
@@ -17,18 +17,24 @@ let DeckReference = firebase.database();
 let deckRef = DeckReference.ref('deckoCards');
 let player1Ref = DeckReference.ref('player1Hand');
 let player2Ref = DeckReference.ref('player2Hand');
-let countRef = DeckReference.ref('count');
-let cribRef = DeckReference.ref('crib');
+let count1Ref = DeckReference.ref('count1');
+let count2Ref = DeckReference.ref('count2');
+let crib1Ref = DeckReference.ref('crib1');
+let crib2Ref = DeckReference.ref('crib2');
 let starterRef = DeckReference.ref('starter');
 
 //set the player hands to empty on page load
 player1Ref.set({player1Cards:[]});
 player2Ref.set({player2Cards:[]});
-countRef.set({count:[]});
-cribRef.set({crib:[]});
+count1Ref.set({count:[]});
+count2Ref.set({count:[]});
+crib1Ref.set({crib:[]});
+crib2Ref.set({crib:[]});
 starterRef.set({starter:[]})
 
-//create card class
+//========================================
+//create card class and deck
+//========================================
 class Card {
   constructor(suit, value){
     this.suit = suit;
@@ -36,7 +42,7 @@ class Card {
   }
 }
 
-//create deck
+//create deck out of Card class
 class Deck {
   constructor() {
     this.deck = [];
@@ -84,10 +90,6 @@ let values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 let cardDeck = new Deck();
 cardDeck.createDeck(suits, values);
 cardDeck.shuffle();
-// console.log(cardDeck);
-//
-// //set cardDeck into Firebase
-// deckRef.set(cardDeck);
 
 //make player hands
 let player1Hand = [];
@@ -97,22 +99,64 @@ let turn = true;
 //get player hands html elements
 let player1El = document.querySelector('.player1');
 let player2El = document.querySelector('.player2');
-let dealButton = document.querySelector('#dealEl');
-let resetButton = document.querySelector('#resetEl');
+
+let $player1El = $('.player1');
+let $player2El = $('.player2');
+
+//get player crib elements
+let player1CribEl = document.querySelector('.crib1');
+let player2CribEl = document.querySelector('.crib2');
+
+let $player1Crib = $('.crib1');
+let $player2Crib = $('.crib2');
+
+let starterEl = document.querySelector('.starter');
+let player1PotEl = document.querySelector('.player1-pot');
+let player2PotEl = document.querySelector('.player2-pot');
 
 // let showButton1 = document.querySelector('.show-hand-1');
 // let showButton2 = document.querySelector('.show-hand-2');
 
-let $player1El = $('.player1');
-let $player2El = $('.player2');
+//get button elements
+let dealButton = document.querySelector('#dealEl');
+let resetButton = document.querySelector('#resetEl');
+
 let $showButton1 = $('.show-hand-1');
 let $showButton2 = $('.show-hand-2');
+let $cribButton1 = $('.show-crib-1');
+let $cribButton2 = $('.show-crib-2');
 
 $showButton1.click(()=>{$player1El.toggleClass('hide')});
 $showButton2.click(()=>{$player2El.toggleClass('hide')});
+$cribButton1.click(()=>{$player1Crib.toggleClass('hide')});
+$cribButton2.click(()=>{$player2Crib.toggleClass('hide')});
 
+
+resetButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  //set the player hands to empty on page load
+  player1Ref.set({player1Cards:[]});
+  player2Ref.set({player2Cards:[]});
+  count1Ref.set({count:[]});
+  count2Ref.set({count:[]});
+  crib1Ref.set({crib:[]});
+  crib2Ref.set({crib:[]});
+  starterRef.set({starter:[]})
+
+  // create a new deck and shuffle it
+  cardDeck = new Deck();
+  cardDeck.createDeck(suits, values);
+  cardDeck.shuffle();
+
+  // reset the player hand arrays and the turn boolean
+  player1Hand = [];
+  player2Hand = [];
+  turn = true;
+})
+
+//call deal function when click on deal button
 dealButton.addEventListener('click', deal);
-
 
 function deal(){
   deckRef.once('value', (snap)=>{
@@ -120,18 +164,26 @@ function deal(){
     let fbCard = fbDeck.shift();
     deckRef.set(fbDeck);
     // console.log(fbCard.suit);
-
-    if(turn){
-      player1Ref.push().set(fbCard);
-      turn = false;
-    } else {
-      player2Ref.push().set(fbCard);
-      turn = true;
+    let val = document.querySelector('input[name=master-controls]:checked').value
+    
+    if(val === 'deal'){
+      if(turn){
+        player1Ref.push().set(fbCard);
+        turn = false;
+      } else {
+        player2Ref.push().set(fbCard);
+        turn = true;
+      }
+    } else if(val === 'starter') {
+      console.log('starter');
+      starterRef.push().set(fbCard);
     }
+    
   });
 };
 
 //listen for change in value of player 1 hand
+//then creates an html element with that suit and value
 player1Ref.on('value', (snap)=>{
   let hand = snap.val();
   //reset the players hand element
@@ -140,6 +192,7 @@ player1Ref.on('value', (snap)=>{
 });
 
 //listen for change in value of player 2 hand
+//then creates an html element with that suit and value
 player2Ref.on('value', (snap)=>{
   let hand = snap.val();
   //reset player hand element
@@ -147,10 +200,55 @@ player2Ref.on('value', (snap)=>{
   getFBHand(player2El, hand);
 })
 
-//this runs every tme there is a change in a player's hand in the database
+starterRef.on('value', (snap)=>{
+  let hand = snap.val();
+  starterEl.innerHTML = '';
+  getFBHand(starterEl, hand);
+})
+
+crib1Ref.on('value', (snap)=>{
+  let hand = snap.val();
+  player1CribEl.innerHTML = '';
+  getFBHand(player1CribEl, hand);
+})
+
+crib2Ref.on('value', (snap)=>{
+  let hand = snap.val();
+  player2CribEl.innerHTML = '';
+  getFBHand(player2CribEl, hand);
+})
+
+count1Ref.on('value', (snap)=>{
+  let hand = snap.val();
+  let val = document.querySelector('input[name="radiogroup1"]:checked').value;
+
+  if(val === 'pot') {
+    player1PotEl.innerHTML = '';
+    getFBHand(player1PotEl, hand);
+  } else if(val === 'crib') {
+    player1CribEl.innerHTML = '';
+    getFBHand(player1CribEl, hand)
+  }
+})
+
+count2Ref.on('value', (snap)=>{
+  let hand = snap.val();
+  let val = document.querySelector('input[name="radio-group"]:checked').value;
+
+  if(val === 'pot') {
+    player2PotEl.innerHTML = '';
+    getFBHand(player2PotEl, hand);
+  } else if(val === 'crib') {
+    player2CribEl.innerHTML = '';
+    getFBHand(player2CribEl, hand)
+  }
+})
+
+//this runs every time there is a change in a player's hand in the database
 //for each key in hand, create HTML element and append to hand element
 function getFBHand(handEl, hand){
   for(key in hand){
+    // console.log(key);
     let suit = hand[key].suit;
     let value = hand[key].value;
     let cardEl = document.createElement('div');
@@ -162,6 +260,91 @@ function getFBHand(handEl, hand){
     handEl.appendChild(cardEl);
   }
 }
+
+player1El.addEventListener('click', (e)=>{
+  //get nearest card element to the target
+  let card = e.target.closest('div.card');
+  // console.log(card);
+  //get the suit and card value for the card that was clicked
+  let suit = card.classList[1];
+  let value = card.querySelector('span').innerHTML;
+  console.log(suit, value);
+
+  card.remove()
+  //get a snapshot of the player's hand search it for the
+  //corresponding card that was clicked
+  player1Ref.once('value', (snap)=>{
+    let hand = snap.val();
+    
+    // console.log(hand);
+    //iterate through the cards in the hand and find the one that was clicked
+    for (key in hand) {
+      // grab the suit and value of the key being checked
+      let fbSuit = hand[key]['suit'];
+      let fbValue = hand[key]['value'];
+      // see if card should go to crib or pot
+      let val = document.querySelector('input[name="radiogroup1"]:checked').value;
+
+      //check if the fb card matches the card clicked
+      if (fbSuit === suit && fbValue.toString() === value) {
+        // executes if the crib radio is selected
+        if(val === 'crib') {
+          crib1Ref.push().set(hand[key]);
+
+        // executes if the pot radio is selected
+        } else if(val === 'pot') {
+          count1Ref.push().set(hand[key]);
+        }
+      } else {
+        console.log('wrong card');
+      } 
+    }
+  })
+})
+
+// Same for player 2 element
+player2El.addEventListener('click', (e)=>{
+  //get nearest card element to the target
+  let card = e.target.closest('div.card');
+  // console.log(card);
+  //get the suit and card value for the card that was clicked
+  let suit = card.classList[1];
+  let value = card.querySelector('span').innerHTML;
+  console.log(suit, value);
+
+  card.remove()
+  //get a snapshot of the player's hand search it for the
+  //corresponding card that was clicked
+  player2Ref.once('value', (snap)=>{
+    let hand = snap.val();
+    
+    // console.log(hand);
+    //iterate through the cards in the hand and find the one that was clicked
+    for (key in hand) {
+      // grab the suit and value of the key being checked
+      let fbSuit = hand[key]['suit'];
+      let fbValue = hand[key]['value'];
+      // see if card should go to crib or pot
+      let val = document.querySelector('input[name="radio-group"]:checked').value;
+
+      //check if the fb card matches the card clicked
+      if (fbSuit === suit && fbValue.toString() === value) {
+        // executes if the crib radio is selected
+        if(val === 'crib') {
+          crib2Ref.push().set(hand[key]);
+
+        // executes if the pot radio is selected
+        } else if(val === 'pot') {
+          count2Ref.push().set(hand[key]);
+        }
+      } else {
+        console.log('wrong card');
+      } 
+    }
+  })
+})
+
+
 
 /*
 
